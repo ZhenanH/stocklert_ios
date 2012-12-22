1788d51a7153dc739fd72f99c6d10c0745a4723c
guoer23 <guoer23@gmail.com>
2012-12-22 14:17:52 -0500

    Initial Commit

---
+++ Stocklert/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Stocklert
+//
+//  Created by Zhenan Hong on 12/22/12.
+//  Copyright (c) 2012 Lean Develop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Stocklert/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Stocklert
+//
+//  Created by Zhenan Hong on 12/22/12.
+//  Copyright (c) 2012 Lean Develop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
